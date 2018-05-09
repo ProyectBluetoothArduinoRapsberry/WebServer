cdcc9279ad161ace12df48ff28ff8e842b4ef242
Diego Rojas <dicrojasch@unal.edu.co>
2018-05-08 23:40:38 -0500

    Conexion e intercambio de mensajes con la raspberry

--- server.js
+++ server.js
@@ -1,30 +1,83 @@
-'use strict';
 
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
+// Authentication module.
+const http = require('http');
+const WebSocket = require('ws');
+var auth = require('http-auth');
+var fs = require('fs');
+var xml2js = require('xml2js');
+var url = require('url');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const wssRasp = new WebSocket.Server({ noServer: true });
+const wssClient = new WebSocket.Server({ noServer: true });
 
-const wss = new SocketServer({ server });
+var clientsExists = false;
+var authenticated = false;
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
+wssRasp.on('connection', function connection(wsr) {
+  wsr.send("test");
+  wsr.on('message', function incoming(message) {
+      wssClient.clients.forEach(function each(ws) {
+        ws.send(message);
+      });
+
+      wssRasp.clients.forEach(function each(ws) {
+        ws.send(message);
+      });
+    console.log('received: %s', message);
   });
-  ws.on('close', () => console.log('Client disconnected'));
 });
 
-
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString() + "test");
+wssClient.on('connection', function connection(wsc) {
+  wsc.on('message', function incoming(message) {
+      wssRasp.clients.forEach(function each(ws) {
+        ws.send(message);
+      });
   });
-}, 1000);
+});
+
+var parser = new xml2js.Parser();
+var basic = auth.basic({realm: "Sistema Control."}, (username, password, callback) => {
+      fs.readFile('./config/users.xml', function(err, data) {
+          parser.parseString(data, function (err, result) {
+            authenticated = false;
+            result.users.user.forEach(function(element) {
+              console.log(element.username[0] + "   " + element.password[0])
+              authenticated = authenticated || (username === element.username[0] && password === element.password[0]);
+            });
+
+          });
+
+          callback(authenticated);
+      });
+
+    }
+);
+
+// Creating new HTTP server.
+const server = http.createServer(basic, (req, res) => {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+
+});
+
+server.on('upgrade', function upgrade(request, socket, head) {
+  const pathname = url.parse(request.url).pathname;
+
+  if (pathname === '/rasp') {
+    wssRasp.handleUpgrade(request, socket, head, function done(ws) {
+      wssRasp.emit('connection', ws, request);
+    });
+  } else if (pathname === '/client') {
+    wssClient.handleUpgrade(request, socket, head, function done(ws) {
+      wssClient.emit('connection', ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(1337);
